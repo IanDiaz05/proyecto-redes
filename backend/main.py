bf12cb2c03eb65674f1980c3bf8a0eae44c4cf34
IanDiaz05 <230301007@ucaribe.edu.mx>
2026-04-28 11:19:13 -0500

    Fix: arrancar servidor uvicorn en main.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
@@ -9,3 +10,8 @@ def read_root():
 @app.get("/api/datos")
 def get_datos():
     return {"datos": ["Aún no hay datos de los agentes"]}
+
+# Esta es la pieza clave que faltaba
+if __name__ == "__main__":
+    # host="0.0.0.0" permite que el tráfico externo/Docker acceda a la API
+    uvicorn.run(app, host="0.0.0.0", port=8000)
